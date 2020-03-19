9e2b7013a78f102d1639cf2999d025c8e0b20e67
eye5onl1 <61664000+eye5onl1@users.noreply.github.com>
2020-03-19 23:35:57 +0500

    Update helloworld.sol

--- helloworld.sol
+++ helloworld.sol
@@ -1,11 +1,29 @@
 pragma solidity ^0.4.24;
 contract test{
-    string name
-    function getName(string x)public { name = x;}
-    function retName() public returns(string) {
+    string name; 
+    
+    // this function is used to set the name 
+    function getName(string x)public 
+    { 
+    name = x;
+    }
+    
+    // this function demonstrates the use of **view** . It is used for a function which is 
+    // read only  
+    function retName() public view returns(string) 
+    {
         return name;
     }
-    function renderHelloWorld () public pure returns (string) {
+    
+    // **HelloWorld** and **Multiply** function makes user of modifier **pure** to indicate that this function returns a value using
+    //only function parameters and does not make use of any external variable 
+    
+    function HelloWorld () public pure returns (string) 
+    {
    return 'helloWorld';
- }
+    }
+    function multiply(uint n1,uint n2) public pure returns(uint)
+    { 
+        return n1*n2;
+    }
 }
